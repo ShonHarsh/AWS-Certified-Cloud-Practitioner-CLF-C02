3310559cc640a060552900db30301350cec913f0
Shon Harsh <shon.harsh@gmail.com>
2024-06-21 13:40:32 -0400

    Add notes to ec2 user data file

--- ec2-fundamentals/ec2-user-data.sh
+++ ec2-fundamentals/ec2-user-data.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Use this for your user data (script from top to bottom)
 # install httpd (Linux 2 version)
+#
+# EC2 Section - ECT User Data
+# Script runs only on first start
+# Configures the server to be a web server
 yum update -y
 yum install -y httpd
 systemctl start httpd
